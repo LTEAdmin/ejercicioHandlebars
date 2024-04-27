82310b0848ddaec30ec22d2d67dc63cbe25fe9ab
Alejandra Flaño <aflanog@latiaencanto.cl>
2024-04-27 18:18:52 -0400

    Instalacion de handlerbar

--- index.js
+++ index.js
@@ -2,3 +2,7 @@ const express = require('express');
 
 const app = express();
 const port = 3003;
+
+app.listen(port, () => {
+    console.log(`Server listening on port http://localhost:${port}`);
+});
