bcf2cacf005caa5e0db906cc2ea099c001f1bf92
Alejandra Flaño <aflanog@latiaencanto.cl>
2024-04-27 18:48:40 -0400

    configuracion de HBS

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const routes = require('../Ejercicio Handlebars/routes/routes.js');
+const exphbs = require('express-handlebars');
+const path = require('path');
 const app = express();
 const port = 3003;
 
+//configuracion de HBS
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname + '/views'));
 
 
 app.listen(port, () => {
