dc85d558945aa3320ac71a8621febd80c0f5ea40
Alejandra Flaño <aflanog@latiaencanto.cl>
2024-04-27 18:22:51 -0400

    creacion de rutas basicas

--- index.js
+++ index.js
@@ -3,6 +3,14 @@ const express = require('express');
 const app = express();
 const port = 3003;
 
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.get('*', (req, res) => {
+    res.status(404).send('esta pagina no existe    ');
+});
+
 app.listen(port, () => {
     console.log(`Server listening on port http://localhost:${port}`);
 });
