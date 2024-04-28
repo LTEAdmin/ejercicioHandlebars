4eb6ef8bbd994d704db78c81c336cc8f5e1bed27
Alejandra Flaño <aflanog@latiaencanto.cl>
2024-04-28 10:01:52 -0400

    ejercicio Handlebar no logro determinar el error

--- index.js
+++ index.js
@@ -1,14 +1,27 @@
 const express = require('express');
-const routes = require('../Ejercicio Handlebars/routes/routes.js');
-const exphbs = require('express-handlebars');
-const path = require('path');
-const app = express();
-const port = 3003;
+const app = express();// require('./routes/routes.js');
+const exphbs = require("express-handlebars");
+const path = require("path");//
+const port = process.env.PORT || 3003;
 
 //configuracion de HBS
-app.set('view engine', 'hbs');
+app.set('view engine', 'handlebars');
 app.set('views', path.join(__dirname + '/views'));
+app.engine('hbs', exphbs.engine({
+    defaultLayout:'main',
+}));
 
+app.get('/home', (req, res) => {
+  res.render('home');
+});
+
+app.get('/about', (req, res) => {
+  res.render('about'); 
+});
+
+app.get("*", (req, res) => {
+  res.status(404).send("esta pagina no existe    ");
+});
 
 app.listen(port, () => {
     console.log(`Server listening on port http://localhost:${port}`);
--- routes/routes.js
+++ routes/routes.js
@@ -1,8 +1,18 @@
-const express = require("express");
+/* const express = require("express");
+const exphbs = require("express-handlebars");
+const path = require("path");
 const app = express();
 
+
+//configuracion de HBS
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname + '/views'));
+app.engine('hbs', exphbs.engine({
+    defaultLayout:'main',
+}));
+
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.render('home');
 });
 
 app.get("/about", (req, res) => {
@@ -13,4 +23,4 @@ app.get("*", (req, res) => {
   res.status(404).send("esta pagina no existe    ");
 });
 
-modules.exports = app;
+module.exports = app; */
