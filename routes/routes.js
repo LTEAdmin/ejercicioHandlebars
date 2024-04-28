da2f0d2c2c89d27ab04067368f8832956f7b1ac3
Alejandra Flaño <aflanog@latiaencanto.cl>
2024-04-28 13:22:53 -0400

    creacion carpeta partials y navbar para reutilizar

--- routes/routes.js
+++ routes/routes.js
@@ -5,22 +5,22 @@ const app = express();
 
 
 //configuracion de HBS
-app.set('view engine', 'hbs');
+app.set('view engine', 'handlebars');
 app.set('views', path.join(__dirname + '/views'));
-app.engine('hbs', exphbs.engine({
+app.engine('handlebars', exphbs.engine({
     defaultLayout:'main',
 }));
 
-app.get("/", (req, res) => {
+app.get('/home', (req, res) => {
   res.render('home');
 });
 
-app.get("/about", (req, res) => {
-  res.sendFile(__dirname + "/about.html"); //('About Page');
+app.get('/about', (req, res) => {
+  res.render('about'); 
 });
 
 app.get("*", (req, res) => {
   res.status(404).send("esta pagina no existe    ");
 });
 
-module.exports = app; */
+module.exports = app;  */
